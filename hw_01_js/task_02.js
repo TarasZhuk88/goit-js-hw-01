5c3211399b5d8c00ec6c7fd855e71cc77effe583
Taras Zhuk <66519608+TarasZhuk88@users.noreply.github.com>
2020-09-18 14:27:58 +0300

    task02Done

--- hw_01_js/task_02.js
+++ hw_01_js/task_02.js
@@ -1,19 +1,8 @@
 const invoice = 100; 
 const stock = 100;
 
-// Write code under this line
-const message = ;
+if (invoice===stock) {message = 'Заказ оформлен, с вами свяжется менеджер';}
+else if (invoice<=stock) {message = 'Заказ оформлен, с вами свяжется менеджер';}
+else if (invoice>stock) {message =  'На складе недостаточно товаров!';}
 
-//console.log(message);
-
-//если invoice равен 100
-// то значение message будет равно
-// 'Заказ оформлен, с вами свяжется менеджер'
-
-//если invoice равен 50
-// то значение message будет равно
-// 'Заказ оформлен, с вами свяжется менеджер'  
-
-//если invoice равен 150
-// то значение message будет равно
-// 'На складе недостаточно товаров!'  
+console.log(message);
